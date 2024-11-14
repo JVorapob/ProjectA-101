a9506cf18b18469e02610e2a2ba547586941ff38
JVorapob <163741502+JVorapob@users.noreply.github.com>
2024-11-14 20:43:12 +0700

    Add files via upload

---
+++ rush/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('div ul li a.Inp').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
